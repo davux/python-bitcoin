23a8c22f19fbc9ca8cb59ec962ce4b7c7149fbdf
davux <da@weeno.net>
2010-11-18 11:46:10 -0600

    Move bitcoin module to a modules/ directory

---
+++ address.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# vi: sts=4 et sw=4
+
+from controller import Controller
+
+class Address(object):
+    '''A Bitcoin address. Bitcoin properties of an address (for example its
+       label) may be read and written like normal Python instance attributes
+       (foo.label, or foo.label="blah").'''
+
+    def __init__(self, address=None):
+        '''Constructor. If address is empty, generate one.'''
+        if address is None:
+            address = Controller().getnewaddress()
+        if not Controller().validateaddress(address)['isvalid']:
+            raise InvalidBitcoinAddressError(address)
+        self.address = address
+
+    def __str__(self):
+        return self.address
+
+    def __getattr__(self, name):
+        if 'label' == name:
+            return Controller().getlabel(self.address)
+
+    def __setattr__(self, name, value):
+        if 'label' == name:
+            if label is None:
+                Controller().setlabel(self.address)
+            else:
+                Controller().setlabel(self.address, label)
+        else:
+            object.__setattr__(self, name, value)
+
+class InvalidBitcoinAddressError(Exception):
+    '''The Bitcoin address is invalid.'''
+    pass
---
+++ controller.py
@@ -0,0 +1,47 @@
+from jsonrpc import ServiceProxy
+
+class Controller(object):
+    '''
+    A connector to a bitcoin daemon. A caching is done so that if the args are the same
+    than a previous call to Controller(...), the previous instance is reused.
+    A call to Controller() without any arguments tries to reuse an arbitrary instance
+    that is already cached, if any. This is particularly useful if you only care about
+    one instance of the connector.
+    '''
+
+    cache = {}
+
+    def __new__(cls, user='', password='', host='127.0.0.1', port=8332, protocol='http'):
+        if ('' == user) and (0 != len(cls.cache)):
+            url = cls.cache.keys()[0]
+        else:
+            url = "%s://%s:%s@%s:%s" % (protocol, user, password, host, port)
+            if not url in cls.cache:
+                cls.cache[url] = object.__new__(cls)
+                cls.cache[url].conn = ServiceProxy(url)
+                cls.cache[url].backupwallet = cls.cache[url].conn.backupwallet
+                cls.cache[url].getaddressesbylabel = cls.cache[url].conn.getaddressesbylabel
+                cls.cache[url].getbalance = cls.cache[url].conn.getbalance
+                cls.cache[url].getblockcount = cls.cache[url].conn.getblockcount
+                cls.cache[url].getblocknumber = cls.cache[url].conn.getblocknumber
+                cls.cache[url].getconnectioncount = cls.cache[url].conn.getconnectioncount
+                cls.cache[url].getdifficulty = cls.cache[url].conn.getdifficulty
+                cls.cache[url].getgenerate = cls.cache[url].conn.getgenerate
+                cls.cache[url].setgenerate = cls.cache[url].conn.setgenerate
+                cls.cache[url].getinfo = cls.cache[url].conn.getinfo
+                cls.cache[url].getlabel = cls.cache[url].conn.getlabel
+                cls.cache[url].setlabel = cls.cache[url].conn.setlabel
+                cls.cache[url].getnewaddress = cls.cache[url].conn.getnewaddress
+                cls.cache[url].getreceivedbyaddress = cls.cache[url].conn.getreceivedbyaddress
+                cls.cache[url].getreceivedbylabel = cls.cache[url].conn.getreceivedbylabel
+                cls.cache[url].help = cls.cache[url].conn.help
+                cls.cache[url].listreceivedbyaddress = cls.cache[url].conn.listreceivedbyaddress
+                cls.cache[url].listreceivedbylabel = cls.cache[url].conn.listreceivedbylabel
+                cls.cache[url].sendtoaddress = cls.cache[url].conn.sendtoaddress
+                cls.cache[url].stop = cls.cache[url].conn.stop
+                cls.cache[url].validateaddress = cls.cache[url].conn.validateaddress
+        return cls.cache[url]
+
+class BitcoinServerIOError(IOError):
+    '''There was a problem when connecting to the bitcoin server'''
+    pass
