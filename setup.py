65766d63ff41221d016281df691d2f3212ff93b0
davux <da@weeno.net>
2010-12-06 12:02:24 -0600

    Make an installable module

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "bitcoin",
+    version = "0.0.1",
+    description = "A high-level bitcoin package.",
+    keywords = "Bitcoin",
+    author = "David Ammouial",
+    url = "http://gitorious.org/python-bitcoin",
+    license = "GPLv2",
+    long_description = """""",
+    packages = ['bitcoin']
+)
