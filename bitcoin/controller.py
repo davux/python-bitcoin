4c7c91797b488a4ec6de114d129e2297929ae66b
davux <da@weeno.net>
2010-12-09 15:14:48 -0600

    Update methods available in most recent version
    Add the following methods:
      - getaccountaddress
      - gethashespersec
      - gettransaction
      - listtransactions
      - sendfrom
      - getwork

--- bitcoin/controller.py
+++ bitcoin/controller.py
@@ -20,6 +20,7 @@ class Controller(object):
                 cls.cache[url] = object.__new__(cls)
                 cls.cache[url].conn = ServiceProxy(url)
                 cls.cache[url].backupwallet = cls.cache[url].conn.backupwallet
+                cls.cache[url].getaccountaddress = cls.cache[url].conn.getaccountaddress
                 cls.cache[url].getaddressesbyaccount = cls.cache[url].conn.getaddressesbyaccount
                 cls.cache[url].getbalance = cls.cache[url].conn.getbalance
                 cls.cache[url].getblockcount = cls.cache[url].conn.getblockcount
@@ -28,8 +29,10 @@ class Controller(object):
                 cls.cache[url].getdifficulty = cls.cache[url].conn.getdifficulty
                 cls.cache[url].getgenerate = cls.cache[url].conn.getgenerate
                 cls.cache[url].setgenerate = cls.cache[url].conn.setgenerate
+                cls.cache[url].gethashespersec = cls.cache[url].conn.gehashespersec
                 cls.cache[url].getinfo = cls.cache[url].conn.getinfo
                 cls.cache[url].getaccount = cls.cache[url].conn.getaccount
+                cls.cache[url].gettransaction = cls.cache[url].conn.gettransaction
                 cls.cache[url].setaccount = cls.cache[url].conn.setaccount
                 cls.cache[url].getnewaddress = cls.cache[url].conn.getnewaddress
                 cls.cache[url].getreceivedbyaddress = cls.cache[url].conn.getreceivedbyaddress
@@ -37,7 +40,10 @@ class Controller(object):
                 cls.cache[url].help = cls.cache[url].conn.help
                 cls.cache[url]._listreceivedbyaddress = cls.cache[url].conn.listreceivedbyaddress
                 cls.cache[url]._listreceivedbyaccount = cls.cache[url].conn.listreceivedbyaccount
+                cls.cache[url].listtransactions = cls.cache[url].conn.listtransactions
+                cls.cache[url].sendfrom = cls.cache[url].conn.sendfrom
                 cls.cache[url].sendtoaddress = cls.cache[url].conn.sendtoaddress
+                cls.cache[url].getwork = cls.cache[url].conn.getwork
                 cls.cache[url].stop = cls.cache[url].conn.stop
                 cls.cache[url].validateaddress = cls.cache[url].conn.validateaddress
         return cls.cache[url]
